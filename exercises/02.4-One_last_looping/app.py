a39fcb815b48089985acb2fdb0f64767bd7e3afa
samvalvi <samuelvalerin@gmail.com>
2021-03-23 20:46:36 -0600

    exercise 3.0

--- exercises/02.4-One_last_looping/app.py
+++ exercises/02.4-One_last_looping/app.py
@@ -1,4 +1,9 @@
 my_sample_list = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fernando','Cesco','Bart','Annie']
 
-#Your code here:
+# Your code here:
+my_sample_list[1] = "Steven"
+my_sample_list[-1] = "Pepe"
+my_sample_list[0] = my_sample_list[2] + my_sample_list[4]
 
+for i in my_sample_list[::-1]:
+    print(i)
